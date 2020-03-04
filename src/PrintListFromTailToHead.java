bc74f86ae5a2a2e672bac5424b65722fff8ca520
foochane <fuchengkm@qq.com>
2020-03-04 14:52:36 +0800

    update

--- src/PrintListFromTailToHead.java
+++ src/PrintListFromTailToHead.java
@@ -4,9 +4,9 @@ import java.util.Stack;
 /**
  * 《剑指Offer_编程题-从尾到头打印链表》
  * 输入一个链表，按链表从尾到头的顺序返回一个ArrayList。
- * <p>
+ *
  * https://www.nowcoder.com/practice/d0267f7f55b3412ba93bd35cfa8e8035
- * <p>
+ *
  * 20200303
  */
 
