948b83618936b3329623675519450b33fad61423
foochane <fuchengkm@qq.com>
2020-04-22 21:46:25 +0800

    _14_链表中倒数第k个结点

---
+++ 剑指Offer编程题/_14_链表中倒数第k个结点/Solution.java
@@ -0,0 +1,16 @@
+package _14_链表中倒数第k个结点;
+
+/**
+ *
+ * 输入一个链表，输出该链表中倒数第k个结点。
+ *
+ * https://www.nowcoder.com/practice/529d3ae5a407492994ad2a246518148a
+ *
+ * 20200409
+ */
+
+public class Solution {
+//    public ListNode FindKthToTail(ListNode head, int k) {
+//
+//    }
+}
