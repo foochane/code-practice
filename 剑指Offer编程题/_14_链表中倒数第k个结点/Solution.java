f726ac41d21eee5bfbaec21c6997c7aea1df3ea3
foochane <fuchengkm@qq.com>
2020-05-15 00:13:24 +0800

    _14_链表中倒数第k个结点

--- 剑指Offer编程题/_14_链表中倒数第k个结点/Solution.java
+++ 剑指Offer编程题/_14_链表中倒数第k个结点/Solution.java
@@ -10,7 +10,61 @@ package _14_链表中倒数第k个结点;
  */
 
 public class Solution {
-//    public ListNode FindKthToTail(ListNode head, int k) {
-//
-//    }
+
+    // 方法一 先统计链表长度，在反推倒数第k个节点是正数第几个节点
+    // 如果链表的长度是n，则倒数第k个节点就是正数第n-k个节点
+    public ListNode FindKthToTail1(ListNode head, int k) {
+        int n;
+
+        if(head == null){
+            return null;
+        }else {
+            n = 1;
+        }
+        ListNode listHead = head;
+        while (listHead.next != null){
+            n++;
+            listHead = listHead.next;
+        }
+        if(k>n){
+            return null;
+        }
+        int i = 1;
+        while (i<=n-k && head != null){
+            head = head.next;
+            i++;
+        }
+        return head;
+    }
+
+    // 方法一 取两个指针，第一个指针在链表的第1个位置，第2个指针在链表的第k+1个位置
+    // 两个指针同时往后移动，第2个指针刚指向null时，第1个节点指向倒数第k个节点
+    // 如果链表的长度是n，则倒数第k个节点就是正数第n-k个节点
+    public ListNode FindKthToTail2(ListNode head, int k) {
+        if(head == null){
+            return null;
+        }
+        ListNode node1 = head;
+        ListNode node2 = head;
+        int i = 1;
+        while (i<=k){
+            node2 = node2.next;
+            if(node2 == null){
+                if(i==k){        // 5,{1,2,3,4,5}
+                    return node1;
+                }
+                return null;
+            }
+            i++;
+        }
+
+
+        while (node2 != null){
+
+            node2 = node2.next;
+            node1 = node1.next;
+        }
+        return node1;
+
+    }
 }
