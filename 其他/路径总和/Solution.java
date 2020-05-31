5c9ddd0c559376b10be1a9aecaaf1cd68f48612f
foochane <fuchengkm@qq.com>
2020-05-31 11:40:45 +0800

    update

---
+++ 其他/路径总和/Solution.java
@@ -0,0 +1,35 @@
+package 路径总和;
+
+/**
+ * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例: 
+ * 给定如下二叉树，以及目标和 sum = 22，
+ *
+ *               5
+ *              / \
+ *             4   8
+ *            /   / \
+ *           11  13  4
+ *          /  \      \
+ *         7    2      1
+ * 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/path-sum
+ *
+ * 20200531
+ */
+public class Solution {
+
+    // 使用递归，遍历整棵树，每遍历一个节点sum-1，如果遍历到叶子节点并且sum==0，则放回true；
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        sum -=root.val;
+        if(root.left == null && root.right == null) return sum == 0;
+        return hasPathSum(root.left, sum) || hasPathSum(root.left, sum);  // 这的 || 运算很关键
+
+    }
+}
---
+++ 其他/路径总和/TreeNode.java
@@ -0,0 +1,11 @@
+package 路径总和;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x){
+        val = x;
+    }
+}
