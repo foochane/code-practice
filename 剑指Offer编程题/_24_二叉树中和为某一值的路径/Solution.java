51dd4f69538b596ca8176b4b0356e96abfe7d5f0
foochane <fuchengkm@qq.com>
2020-06-07 22:43:38 +0800

    update

--- 剑指Offer编程题/_24_二叉树中和为某一值的路径/Solution.java
+++ 剑指Offer编程题/_24_二叉树中和为某一值的路径/Solution.java
@@ -10,17 +10,23 @@ package _24_二叉树中和为某一值的路径;
  */
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Solution {
-    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int target) {
-
-    }
-
-    public boolean hasPathSum(TreeNode root, int sum) {
-        if(root == null) return false;
-        sum -=root.val;
-        if(root.left == null && root.right == null) return sum == 0;
-        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);  // 这的 || 运算很关键
-
+    private ArrayList<ArrayList<Integer>> paths = new ArrayList<>();
+    private Stack<Integer> path = new Stack();
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+//      递归终止条件
+        if(root == null)    return paths;
+        path.push(root.val);
+        target -= root.val;
+//      当前路径遍历结束时若节点值之和等于Target，则将该路径加入正确路径队列；
+        if(target == 0 && root.left == null && root.right == null)
+            paths.add(new ArrayList<Integer>(path));
+        FindPath(root.left, target);
+        FindPath(root.right, target);
+//      当前节点判断结束，回溯；
+        path.pop();
+        return paths;
     }
 }
