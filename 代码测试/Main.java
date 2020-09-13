ba53eb47713e0f64c38169ab309d935a8a777c38
foochane <fuchengkm@qq.com>
2020-09-13 19:52:19 +0800

    update

--- 代码测试/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Main{
-    public static void main(String[] args){
-        String s = "222";
-        String s1 = "222";
-        System.out.println(true);
-    }
-}
