1485000937796edc99b5d13decfa905cfe040b6c
foochane <fuchengkm@qq.com>
2020-10-11 16:41:41 +0800

    update 。

--- 知识点/运行顺序/Main.java
+++ 知识点/运行顺序/Main.java
@@ -1,31 +1,39 @@
 package 运行顺序;
 
+/**
+ * 父类静态方法
+ * 子类静态方法
+ * 父类非静态方法
+ * 父类构造方法
+ * 子类非静态方法
+ * 子类构造方法
+ */
 class Main{
     static class Father{
         static {
-            System.out.println("Static Father");
+            System.out.println("父类静态方法");
         }
 
         {
-            System.out.println("NON- STATIC Father");
+            System.out.println("父类非静态方法");
         }
 
         public Father(){
-            System.out.println("Construct father");
+            System.out.println("父类构造方法");
         }
     }
     static class Son extends  Father{
         static {
-            System.out.println("STatci son");
+            System.out.println("子类静态方法");
         }
 
         {
-            System.out.println("NON static son");
+            System.out.println("子类非静态方法");
         }
 
 
         public Son(){
-            System.out.println("constaru son");
+            System.out.println("子类构造方法");
         }
     }
 
